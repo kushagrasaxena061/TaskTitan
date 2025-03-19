6fbec7c66ac5828266fb0eb1be47fa59ac0779e7
Kushagra Saxena <kushagrasaxena061@gmail.com>
2025-03-20 01:59:10 +0530

    bugs fixed

--- app/layout.js
+++ app/layout.js
@@ -34,16 +34,12 @@ export default function RootLayout({ children }) {
       }}
     >
       <html lang="en">
-        <body className={`${inter.className} animated-dotted-background`}>
+        <body className={`${inter.className} `}>
           <ThemeProvider attribute="class" defaultTheme="dark">
             <Header />
             <main className="min-h-screen">{children}</main>
             <Toaster richColors />
-            <footer className="bg-gray-900 py-12">
-              <div className="container mx-auto px-4 text-center text-gray-200">
-                <p>Made with 💗 by RoadsideCoder</p>
-              </div>
-            </footer>
+
           </ThemeProvider>
         </body>
       </html>
--- app/page.js
+++ app/page.js
@@ -9,47 +9,8 @@ import {
 } from "lucide-react";
 import { Button } from "@/components/ui/button";
 import { Card, CardContent } from "@/components/ui/card";
-import {
-  Accordion,
-  AccordionContent,
-  AccordionItem,
-  AccordionTrigger,
-} from "@/components/ui/accordion";
-import CompanyCarousel from "@/components/company-carousel";
-import Image from "next/image";
 
-const faqs = [
-  {
-    question: "What is ZCRUM?",
-    answer:
-      "ZCRUM is a powerful project management tool designed to help teams organize, track, and manage their work efficiently. It combines intuitive design with robust features to streamline your workflow and boost productivity.",
-  },
-  {
-    question: "How does ZCRUM compare to other project management tools?",
-    answer:
-      "ZCRUM offers a unique combination of intuitive design, powerful features, and flexibility. Unlike other tools, we focus on providing a seamless experience for both agile and traditional project management methodologies, making it versatile for various team structures and project types.",
-  },
-  {
-    question: "Is ZCRUM suitable for small teams?",
-    answer:
-      "Absolutely! ZCRUM is designed to be scalable and flexible. It works great for small teams and can easily grow with your organization as it expands. Our user-friendly interface ensures that teams of any size can quickly adapt and start benefiting from ZCRUM's features.",
-  },
-  {
-    question: "What key features does ZCRUM offer?",
-    answer:
-      "ZCRUM provides a range of powerful features including intuitive Kanban boards for visualizing workflow, robust sprint planning tools for agile teams, comprehensive reporting for data-driven decisions, customizable workflows, time tracking, and team collaboration tools. These features work seamlessly together to enhance your project management experience.",
-  },
-  {
-    question: "Can ZCRUM handle multiple projects simultaneously?",
-    answer:
-      "Yes, ZCRUM is built to manage multiple projects concurrently. You can easily switch between projects, and get a bird's-eye view of all your ongoing work. This makes ZCRUM ideal for organizations juggling multiple projects or clients.",
-  },
-  {
-    question: "Is there a learning curve for new users?",
-    answer:
-      "While ZCRUM is packed with features, we've designed it with user-friendliness in mind. New users can quickly get up to speed thanks to our intuitive interface, helpful onboarding process, and comprehensive documentation.",
-  },
-];
+import Image from "next/image";
 
 const features = [
   {
@@ -79,16 +40,7 @@ export default function Home() {
       <section className="container mx-auto py-20 text-center">
         <h1 className="text-6xl sm:text-7xl lg:text-8xl font-extrabold gradient-title pb-6 flex flex-col">
           Streamline Your Workflow <br />
-          <span className="flex mx-auto gap-3 sm:gap-4 items-center">
-            with
-            <Image
-              src={"/logo2.png"}
-              alt="Zscrum Logo"
-              width={400}
-              height={80}
-              className="h-14 sm:h-24 w-auto object-contain"
-            />
-          </span>
+
         </h1>
         <p className="text-xl text-gray-300 mb-10 max-w-3xl mx-auto">
           Empower your team with our intuitive project management solution.
@@ -127,31 +79,7 @@ export default function Home() {
       </section>
 
       {/* Companies Carousel */}
-      <section className="py-20">
-        <div className="container mx-auto">
-          <h3 className="text-3xl font-bold mb-12 text-center">
-            Trusted by Industry Leaders
-          </h3>
-          <CompanyCarousel />
-        </div>
-      </section>
 
-      {/* FAQ Section */}
-      <section className="bg-gray-900 py-20 px-5">
-        <div className="container mx-auto">
-          <h3 className="text-3xl font-bold mb-12 text-center">
-            Frequently Asked Questions
-          </h3>
-          <Accordion type="single" collapsible className="w-full">
-            {faqs.map((faq, index) => (
-              <AccordionItem key={index} value={`item-${index}`}>
-                <AccordionTrigger>{faq.question}</AccordionTrigger>
-                <AccordionContent>{faq.answer}</AccordionContent>
-              </AccordionItem>
-            ))}
-          </Accordion>
-        </div>
-      </section>
 
       {/* CTA Section */}
       <section className="py-20 text-center px-5">
@@ -160,7 +88,7 @@ export default function Home() {
             Ready to Transform Your Workflow?
           </h3>
           <p className="text-xl mb-12">
-            Join thousands of teams already using ZCRUM to streamline their
+            Join thousands of teams already using Tasktitan to streamline their
             projects and boost productivity.
           </p>
           <Link href="/onboarding">
@@ -170,6 +98,7 @@ export default function Home() {
           </Link>
         </div>
       </section>
-    </div>
+    </div >
+
   );
 }
