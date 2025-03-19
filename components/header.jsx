d88ffcf999c2f402a45926cbf31bd564dfb4efaa
Kushagra Saxena <kushagrasaxena061@gmail.com>
2025-03-20 02:47:28 +0530

    bugs fixed

--- components/header.jsx
+++ components/header.jsx
@@ -18,7 +18,7 @@ async function Header() {
           <h1 className="text-2xl font-bold">
             <Image
               src={"/logo2.png"}
-              alt="Zscrum Logo"
+              alt="Logo missing"
               width={200}
               height={56}
               className="h-10 w-auto object-contain"
